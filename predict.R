610d8f9579ae12c8a9ec3649d4320ce96da57742
swosti99 <swosti@ucdavis.edu>
2019-11-24 02:02:50 -0800

    playing around with regtools

--- predict.R
+++ predict.R
@@ -1,31 +1,31 @@
-# starting the project
-source("knn.R")
-source("linear.R")
+# loading regtools and our own functions 
+source("traintestsplit.R")
 library(regtools)
-model_options <- list(test, cluster)
-predictWeather <- function() {
-  n <- 0
-  while(n > 2 || n < 1) {
-    n <- as.integer(readline("Model choice ( KNN/linear [1/2] ) ? :"))
-  }
-  model <- model_options[[n]]
-  print(model(4))
-}
 
+# load the dataset
 data(day1)
+
+model_options <- list(basicKNN, knnest)
+# predictWeather <- function() {
+#   
+# }
+
+
 # print(day1[floor(runif(10, 1, 100)),])
+
 pe1 <- day1[c('temp', 'atemp', 'hum', 'windspeed', 'registered', 'tot')]
+splitData(pe1, 0.7)
 # pe1 <- day1
 # print(peDumms[1:5, ])
 # print(model(3))
 # print(predictWeather())
 # data(peDumms)
 # pe1 <- peDumms[c('age','educ.14','educ.16','sex.1','wageinc','wkswrkd')]
-lmout <- lm(temp ~ .,data=pe1)
-# print(lmout)
-xd <- preprocessx(pe1[,-5],2)  # prep for k-NN, k <= 10
-knnbasic <- basicKNN(pe1, pe1$temp, pe1, 2)
-knnout <- knnest(pe1$temp,xd,2)
+lmout <- lm(temp ~ .,data=trainData)
+lmout <- predict.lm(object = lmout, newdata = testData)
+plot(testData, lmout)
+knnTrain <- preprocessx(trainData[,],2)  # prep for k-NN, k <= 10
+knnbasic <- basicKNN(trainData, trainData$temp, testData, 2)
+# knnout <- knnest(knnTrain$temp,knnTrain,2)
 # print(pe1$hum[1:5])
-parvsnonparplot(knnbasic,knnout)
-?parvsnonparplot
+parvsnonparplot(knnout,knnout)
