d135ed4b57fb57e1186ed7483df1672bcfb20977
swosti99 <swosti@ucdavis.edu>
2019-11-24 17:54:57 -0800

    changed plot types and error calc

--- predict.R
+++ predict.R
@@ -11,21 +11,23 @@ data(day1)
 # TODO : check if clustering works to detect season or month info 
 #       (x = data[-"season"], y = data"season")
 model_options <- list(basicKNN, knnest)
-# predictWeather <- function() {
-#   
-# }
 
 
 # print(day1[floor(runif(10, 1, 100)),])
 
-pe1 <- day1[c('temp', 'atemp', 'hum', 'windspeed', 'registered', 'tot')]
-splitData(pe1, 0.7)
-lmout <- lm(temp ~ .,data=trainData)
-lmout <- predict.lm(object = lmout, newdata = testData[-1])
-plot(testData[,"temp"], lmout)
-knnTrain <- preprocessx(trainData[,],2)  # prep for k-NN, k <= 10
-knnbasic <- basicKNN(trainData, testData$temp, testData, 4)
-# knnout <- knnest(knnTrain$temp,knnTrain,2)
-# parvsnonparplot(knnbasic,knnout)
-# print(knnbasic)
-# plot(knnbasic[1][c('temp')], knnbasic[2][c('temp')])
+predictWeather <- function() {
+  pe1 <- day1[c('temp', 'atemp', 'hum', 'windspeed', 'registered', 'tot')]
+  splitData(pe1, 0.8)
+  lmout <- lm(temp ~ .,data=trainData)
+  # plot(lmout)
+  lmout <- predict.lm(object = lmout, newdata = testData[-1])
+  error <- (mean( abs((testData[,"temp"] - lmout)/lmout) ))
+  # plot(1:length(lmout), testData[, "temp"] - lmout, "h") # to plot out erros
+  
+  # plot should look close to y = x if working correctly
+  plot(testData[,"temp"], lmout,"p", xlab = 'ground truth', ylab = 'linear prediction', main = 'linear model performance ', sub = paste0('Error (%) : ',error))
+  knnTrain <- preprocessx(trainData[,],2)  # prep for k-NN, k <= 10
+  knnbasic <- basicKNN(trainData, testData$temp, testData, 4)
+}
+
+predictWeather()
