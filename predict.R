506e60b11ec1e3afd8712b52893d8996f7c90e5a
swosti99 <swosti@ucdavis.edu>
2019-11-26 16:47:50 -0800

    added some TODO items

--- predict.R
+++ predict.R
@@ -1,33 +1,27 @@
 # loading regtools and our own functions 
 source("traintestsplit.R")
+source("predicttemp.R")
+source("predictseason.R")
 library(regtools)
 
 # load the dataset
 data(day1)
+pe1 <- day1[c('season' ,'yr', 'mnth', 'weathersit',  'temp', 'atemp', 'hum', 'windspeed')]
+
 
 # TODO : a function to test and compare two models
 # TODO : a function to graph data about two models
 # TODO : a function to show correlations between different features of data
 # TODO : check if clustering works to detect season or month info 
-#       (x = data[-"season"], y = data"season")
+#        (x = data[-"season"], y = data"season")
+# TODO : check collinearity among the variables and also graph
+#        the correlation matrix
+# TODO : analysis of the data
+# TODO : make a markov model to predict weather based on data from i days 
+#        to predict tomorrow's weather
+# *windspeed may be important
 model_options <- list(basicKNN, knnest)
 
 
-# print(day1[floor(runif(10, 1, 100)),])
-
-predictWeather <- function() {
-  pe1 <- day1[c('temp', 'atemp', 'hum', 'windspeed', 'registered', 'tot')]
-  splitData(pe1, 0.8)
-  lmout <- lm(temp ~ .,data=trainData)
-  # plot(lmout)
-  lmout <- predict.lm(object = lmout, newdata = testData[-1])
-  error <- mean( abs(testData[,"temp"] - lmout)/lmout )
-  # plot(1:length(lmout), testData[, "temp"] - lmout, "h") # to plot out erros
-  
-  # plot should look close to y = x if working correctly
-  plot(testData[,"temp"], lmout,"p", xlab = 'ground truth', ylab = 'linear prediction', main = 'linear model performance ', sub = paste0('Error (%) : ',error))
-  knnTrain <- preprocessx(trainData[,],2)  # prep for k-NN, k <= 10
-  knnbasic <- basicKNN(trainData, testData$temp, testData, 4)
-}
-
-predictWeather()
+# predictTemp()
+predictSeason(pe1)
---
+++ predicttemp.R
@@ -0,0 +1,16 @@
+predictTemp <- function(pe1) {
+  splitData(pe1, 0.8)
+  
+  # print(names(day1))
+  # print(day1$dteday)
+  lmout <- lm(temp ~ .,data=trainData)
+  # plot(lmout)
+  lmout <- predict.lm(object = lmout, newdata = testData[-1])
+  error <- mean( abs(testData[,"temp"] - lmout)/lmout )
+  plot(1:length(lmout), 100*(testData[, "temp"] - lmout)/lmout, "h") # to plot out erros
+  
+  # plot should look close to y = x if working correctly
+  # plot(testData[,"temp"], lmout,"p", xlab = 'ground truth', ylab = 'linear prediction', main = 'linear model performance ', sub = paste0('Error (%) : ',error))
+  knnTrain <- preprocessx(trainData,2)  # prep for k-NN, k <= 10
+  knnbasic <- basicKNN(trainData, testData$temp, testData, 4)
+}
